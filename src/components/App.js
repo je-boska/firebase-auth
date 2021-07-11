f51923ef8425da6c3c85f57143d84a572cdc70fa
joneirikboska <joneirikboska@gmail.com>
2021-07-11 22:00:02 +0200

    Set up router, create components for dashboard and login.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
 import Signup from './Signup'
+import Dashboard from './Dashboard'
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './Login'
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className='d-flex align-items-center justify-content-center'
-        style={{ minHeight: '100vh' }}>
-        <div className='w-100' style={{ maxWidth: '400px' }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className='d-flex align-items-center justify-content-center'
+      style={{ minHeight: '100vh' }}>
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route path='/' component={Dashboard} exact />
+              <Route path='/signup' component={Signup} />
+              <Route path='/login' component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   )
 }
 
