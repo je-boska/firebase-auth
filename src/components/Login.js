f99b2aeacaa888fd6895bb9a157118159275d2f0
joneirikboska <joneirikboska@gmail.com>
2021-07-11 22:02:33 +0200

    Create form in login component, non-functioning.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,62 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Alert, Card, Button, Form } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Login() {
-  return <div>Login</div>
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const { signup } = useAuth()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setError('')
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+    } catch {
+      setError('Failed to create an acccount')
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Log In</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <Form onSubmit={e => handleSubmit(e)}>
+            <Form.Group id='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                className='mb-2'
+                type='email'
+                required
+                ref={emailRef}
+              />
+            </Form.Group>
+            <Form.Group id='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                className='mb-2'
+                type='password'
+                required
+                ref={passwordRef}
+              />
+            </Form.Group>
+            <Button disabled={loading} className='w-100 mt-2' type='submit'>
+              Log In
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>New here? Sign up</div>
+    </>
+  )
 }
